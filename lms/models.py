ac97c499a372eb338814a4cf9e95128b7bd3839b
dyadyasawa <dyadyasawa@gmail.com>
2024-05-19 16:21:53 +0300

    Подключение статистики, создание и применение миграций.

--- lms/models.py
+++ lms/models.py
@@ -18,13 +18,14 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     title = models.CharField(max_length=50, verbose_name="Название")
-    description = models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
     preview = models.ImageField(
         upload_to="lms/lessons", verbose_name="Превью", blank=True, null=True
     )
     url = models.CharField(
         max_length=100, verbose_name="Ссылка на видео", blank=True, null=True
     )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Урок"
