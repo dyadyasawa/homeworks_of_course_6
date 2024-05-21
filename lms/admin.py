e29d5b26a3cde22f3a97c17112fa73a625cc1be0
dyadyasawa <dyadyasawa@gmail.com>
2024-05-21 10:26:08 +0300

    Добавление моделей в админку.

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from lms.models import Course, Lesson
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+    )
+    list_filter = ("title",)
+    search_fields = (
+        "title",
+    )
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    list_filter = ("title",)
+    search_fields = ("title",)
