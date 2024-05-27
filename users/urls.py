2c1fbd34883a19d541bc6ac9fd01b595ad80416e
dyadyasawa <dyadyasawa@gmail.com>
2024-05-27 21:23:23 +0300

    Правки.

--- users/urls.py
+++ users/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     # path("payment/delete/<int:pk>/", PaymentsDestroyApiView.as_view(), name="payment_delete"),
 
     path("register/", UserCreateAPIView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(permission_classes=AllowAny,), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(permission_classes=AllowAny,), name="token_refresh"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
 ]
 
 # urlpatterns += router.urls
