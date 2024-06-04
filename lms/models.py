0819e5d9a832778d813359a86662e3a06f2c80b1
dyadyasawa <dyadyasawa@gmail.com>
2024-06-04 22:08:13 +0300

    Создание модели подписки.

--- lms/models.py
+++ lms/models.py
@@ -37,3 +37,15 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+
+    def __str__(self):
+        return f'{self.user}: ({self.course})'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
