7d6915975184d07c0a42c755c14f322394a38c96
dyadyasawa <dyadyasawa@gmail.com>
2024-06-05 14:51:52 +0300

    Начало работы с тестированием.

--- lms/tests.py
+++ lms/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from lms.models import Course, Lesson
+from users.models import User
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="admin@example.com")
+        self.course = Course.objects.create(title="Математика", description="Точная наука")
+        self.lesson = Lesson.objects.create(title="Урок_1", description="Введение", course=self.course, owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse("lms:lesson_detail", args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
