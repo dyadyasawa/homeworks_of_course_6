305807195ac45fb7668aa89e732609bfb384226a
dyadyasawa <dyadyasawa@gmail.com>
2024-06-05 16:07:04 +0300

    Тестирование для Lesson.

--- lms/tests.py
+++ lms/tests.py
@@ -22,3 +22,71 @@ class LessonTestCase(APITestCase):
         self.assertEqual(
             response.status_code, status.HTTP_200_OK
         )
+        self.assertEqual(
+            data.get("title"), self.lesson.title
+        )
+
+    def test_lesson_create(self):
+        url = reverse("lms:lesson_create")
+        data = {
+            "title": "Урок_10"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Lesson.objects.filter(title="Урок_1").count(), 1
+        )
+
+    def test_lesson_update(self):
+        url = reverse("lms:lesson_update", args=(self.lesson.pk,))
+        data = {
+            "title": "Урок_10"
+        }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("title"), "Урок_10"
+        )
+
+    def test_lesson_delete(self):
+        url = reverse("lms:lesson_delete", args=(self.lesson.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+        self.assertEqual(
+            Lesson.objects.all().count(), 0
+        )
+
+    def test_lesson_list(self):
+        url = reverse("lms:lesson_list")
+        response = self.client.get(url)
+        data = response.json()
+        # print(data)
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        result = {
+                  "count": 1,
+                  "next": None,
+                  "previous": None,
+                  "results": [
+                             {
+                              "id": self.lesson.pk,
+                              "title": self.lesson.title,
+                              "description": self.lesson.description,
+                              "preview": None,
+                              "url": None,
+                              "course": self.course.pk,
+                              "owner": self.user.pk
+                              }
+                              ]
+                }
+        self.assertEqual(
+            data, result
+        )
