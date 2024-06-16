d250b3aec0af307400cd4e8f34b3128196120679
dyadyasawa <dyadyasawa@gmail.com>
2024-06-16 09:55:11 +0300

    Подключение celery. Настройки.

--- config/settings.py
+++ config/settings.py
@@ -162,3 +162,13 @@ SIMPLE_JWT = {
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+# Celery Configuration Options
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = "redis://localhost:6379"
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
