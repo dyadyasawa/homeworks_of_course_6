ae7484b45b5c1ae07ffcdf8408e336acf0bf7527
dyadyasawa <dyadyasawa@gmail.com>
2024-06-17 15:34:12 +0300

    Правки.

--- lms/views.py
+++ lms/views.py
@@ -29,20 +29,6 @@ class CourseViewSet(ModelViewSet):
             self.permission_classes = (~IsModerators | IsOwner,)
         return super().get_permissions()
 
-    # @action(detail=True, methods=("patch",))
-    # def update_date(self, pk, request, *args, **kwargs):
-        # partial = kwargs.pop('partial', False)
-        # instance = self. get_object()
-        # date = instance.last_update_date
-        # print(date)
-        # instance.last_update_date = datetime.datetime.now()
-        # serializer = self.get_serializer(instance, data=request.data)  #, partial=partial)
-        # serializer.is_valid(raise_exception=True)
-        # self.perform_update(serializer)
-        # sending_mail.delay(instance.id, date)
-        # return Response(serializer.data)
-
-    # @action(detail=False, methods=("patch",))
     def perform_update(self, serializer):
         instance = serializer.save()
         privet.delay(instance.id)
